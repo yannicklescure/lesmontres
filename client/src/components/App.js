e3a40bff5e542f4bb7c661d6332770b14c28e62e
Graham <gtompkins1993@gmail.com>
2022-03-29 23:19:54 -0400

    hey

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,12 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <div>Hello</div>
+      <p>Heya</p>
+    </>
+  );
 }
 
 export default App;
