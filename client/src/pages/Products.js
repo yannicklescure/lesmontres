7394c466932077cdc70b1ce10f8b0debc5eb1967
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-04-01 14:14:54 -0400

    created and worked on item cards

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -10,13 +10,10 @@ import usePersistedState from "../hooks/usePersistedState";
 const Products = () => {
   const params = useParams();
   console.log(params);
-  const category = params?.category !== undefined ? params.category : 'fitness'
-  
+  const category = params?.category !== undefined ? params.category : "fitness";
+
   const {
-    state: {
-      hasLoaded,  
-      items,
-    }
+    state: { hasLoaded, items },
   } = useContext(ItemsContext);
 
   const [allProducts, setAllProducts] = useState([]);
@@ -30,20 +27,21 @@ const Products = () => {
       .then((res) => res.json())
       .then((response) => {
         console.log(response);
-        setCompaniesIds(response.data.map(item => item._id));
+        setCompaniesIds(response.data.map((item) => item._id));
         setCompanies(response.data);
       })
       .catch((err) => console.log(err));
 
-    const tmp = items.filter(item => item.category.toLowerCase() === category);
+    const tmp = items.filter(
+      (item) => item.category.toLowerCase() === category
+    );
     setAllProducts(tmp);
     setProducts(tmp);
     // eslint-disable-next-line
-  }, [category])
-
+  }, [category]);
 
   if (!hasLoaded) {
-    return <Loading size="32" />
+    return <Loading size="32" />;
   }
 
   console.log(category);
@@ -56,9 +54,8 @@ const Products = () => {
       copy.push(company._id);
       setCompaniesIds(copy);
       // console.log(copy);
-    }
-    else {
-      const position = copy.findIndex(id => id === company._id);
+    } else {
+      const position = copy.findIndex((id) => id === company._id);
       // console.log(position);
       copy.splice(position, 1);
       setCompaniesIds(copy);
@@ -66,55 +63,126 @@ const Products = () => {
     }
     // console.log(copy.length);
     const productsToDisplay = [];
-    copy.forEach(id => {
-      const filteredProducts = allProducts.filter(product => product.companyId === id);
-      filteredProducts.forEach(filteredProduct => productsToDisplay.push(filteredProduct));
+    copy.forEach((id) => {
+      const filteredProducts = allProducts.filter(
+        (product) => product.companyId === id
+      );
+      filteredProducts.forEach((filteredProduct) =>
+        productsToDisplay.push(filteredProduct)
+      );
     });
     console.log(productsToDisplay);
-    setProducts(productsToDisplay.sort((a,b) => a._id - b._id));
-  }
+    setProducts(productsToDisplay.sort((a, b) => a._id - b._id));
+  };
 
   const getCompanyName = (id) => {
-    return companies.find(company => company._id === id).name;
-  }
+    return companies.find((company) => company._id === id).name;
+  };
 
-  if (companies.length === 0) return <Loading />
+  if (companies.length === 0) return <Loading />;
 
   return (
-    <Wrapper>
+    <PageWrapper>
       <Sidebar companies={companies} handleChecked={handleChecked} />
-      <Container>
-        {
-          products.map(product => (
-            <div key={product._id}>
-              <StyledImg src={product.imageSrc} alt={product._id} width="96" height="96" />
-              <div>{getCompanyName(product.companyId)}</div>
-            </div>
-          ))
-        }
-      </Container>
-    </Wrapper>
-  )
-}
+      <ProductsSection>
+        {products.map((product) => (
+          // <ProductWrapper>
+          <ProductWrapper>
+            <ImgWrapper>
+              <StyledImg
+                key={product._id}
+                src={product.imageSrc}
+                alt={product._id}
+              />
+            </ImgWrapper>
+            <Description>
+              <ItemName>{product.name}</ItemName>
+              <CompanyName>{getCompanyName(product.companyId)}</CompanyName>
+              <Price>{product.price}</Price>
+            </Description>
+          </ProductWrapper>
+          // </ProductWrapper>
+        ))}
+      </ProductsSection>
+    </PageWrapper>
+  );
+};
 
-const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: 200px auto;
+const PageWrapper = styled.div`
+  /* border: 2px solid pink; */
+  /* display: grid;
+  grid-template-columns: 200px auto; */
+  display: flex;
   position: relative;
   margin-bottom: 16px;
   border-bottom: 1px solid ${COLORS.secondary};
+  background-color: white;
+  /* padding: 50px; */
 `;
 
-const Container = styled.div`
+const ProductsSection = styled.div`
+  /* border-bottom: 1px solid ${COLORS.secondary}; */
+  /* border: 2px solid green; */
   display: flex;
   flex-wrap: wrap;
   padding: 16px;
-`
+  justify-content: center;
+  gap: 30px;
+`;
+
+const ProductWrapper = styled.div`
+  border: 1px solid ${COLORS.grey};
+  width: 275px;
+  height: 350px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  gap: 20px;
+`;
+
+const ImgWrapper = styled.div`
+  /* border: 1px solid ${COLORS.grey}; */
+`;
 
 const StyledImg = styled.img`
-  /* width: ${({width}) => width + 'px'}; */
-  /* height: ${({height}) => height + 'px'}; */
-  object-fit: contain;
+  /* object-fit: contain; */
+  /* border: 2px solid blue; */
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  padding: 30px;
+  /* margin-bottom: 20px; */
+`;
+
+const Description = styled.div`
+  /* border: 2px solid purple; */
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const ItemName = styled.div`
+  padding: 0 15px 0 30px;
+  color: ${COLORS.secondary};
+  /* text-transform: uppercase; */
+  font-size: 14px;
+  font-family: Lato, sans-serif;
+  font-weight: bold;
+  /* letter-spacing: 1px; */
+`;
+
+const CompanyName = styled.div`
+  text-transform: uppercase;
+  font-size: 12px;
+  font-family: Lato, sans-serif;
+`;
+
+const Price = styled.div`
+  font-family: Poppins, sans-serif;
+  font-weight: bold;
+  font-size: 18px;
 `;
 
 export default Products;
