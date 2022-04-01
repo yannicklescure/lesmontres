1a888285654e5168da4fe9f68e1ae9d7651c930e
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-04-01 11:43:36 -0400

    work in progress

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -7,45 +7,47 @@ import { ItemsContext } from "../contexts/ItemsContext";
 const Products = () => {
   const params = useParams();
   console.log(params);
-  const category = params?.category !== undefined ? params.category : 'fitness'
-  
+  const category = params?.category !== undefined ? params.category : "fitness";
+
   const {
-    state: {
-      hasLoaded,  
-      items,
-    }
+    state: { hasLoaded, items },
   } = useContext(ItemsContext);
 
-
   if (!hasLoaded) {
-    return <Loading size="32" />
+    return <Loading size="32" />;
   }
 
   console.log(category);
-  const products = items.filter(item => item.category.toLowerCase() === category);
+  const products = items.filter(
+    (item) => item.category.toLowerCase() === category
+  );
 
   return (
     <Wrapper>
-      {
-        products.map(product => (
-          <div key={product._id}>
-            <StyledImg src={product.imageSrc} alt={product._id} width="96" height="96" />
-          </div>
-        ))
-      }
+      {products.map((product) => (
+        <div key={product._id}>
+          <StyledImg
+            src={product.imageSrc}
+            alt={product._id}
+            width="96"
+            height="96"
+          />
+        </div>
+      ))}
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   padding: 16px;
+  background-color: white;
 `;
 
 const StyledImg = styled.img`
-  /* width: ${({width}) => width + 'px'}; */
-  /* height: ${({height}) => height + 'px'}; */
+  /* width: ${({ width }) => width + "px"}; */
+  /* height: ${({ height }) => height + "px"}; */
   object-fit: contain;
 `;
 
