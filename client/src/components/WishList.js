c481ca51e36d6b617932c7809bb9bbb46ebccb1c
Greg Rodriguez <1965gregory@gmail.com>
2022-04-02 15:36:11 -0400

    partial changes

---
+++ client/src/components/WishList.js
@@ -0,0 +1,8 @@
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { WishListContext } from "../contexts/WishListContext";
+
+const WishList = () => {
+  return <h1>Hello from deep on your cart</h1>;
+};
+export default WishList;
---
+++ client/src/components/WishListBar.js
@@ -0,0 +1,47 @@
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+import styled from "styled-components";
+import { AiOutlineClose } from "react-icons/ai";
+import { COLORS } from "../constants";
+import { WishListContext } from "../contexts/WishListContext";
+
+const WishListBar = () => {
+  const {
+    state: { isWishListBarOpen },
+    actions: { openWishListBar, closeWishListBar },
+  } = useContext(WishListContext);
+
+  return (
+    isWishListBarOpen && (
+      <WishListBarContainer>
+        <CloseIcon
+          color={`${COLORS.dark}`}
+          onClick={() => {
+            closeWishListBar();
+          }}
+        />
+      </WishListBarContainer>
+    )
+  );
+};
+export default WishListBar;
+
+const WishListBarContainer = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  background: white;
+  width: 30%;
+  z-index: 20;
+  transition: top ease 5s;
+  box-shadow: 0px 68px 207px 200px rgba(0, 0, 0, 0.65);
+`;
+
+const CloseIcon = styled(AiOutlineClose)`
+  font-size: 18px;
+  position: absolute;
+  right: 3%;
+  top: 25px;
+  cursor: pointer;
+`;
