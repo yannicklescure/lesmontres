618587766459cfc77b6261a88fa1807a4868e215
Yannick Lescure <yannick.lescure@gmail.com>
2022-04-01 21:55:01 -0400

    improve UX/UI

--- client/src/components/Sidebar/Checkbox.js
+++ client/src/components/Sidebar/Checkbox.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { COLORS } from "../../constants";
 
 const Checkbox = ({company, handleChecked}) => {
   const [checked, setChecked] = useState(false);
@@ -28,6 +29,16 @@ const Checkbox = ({company, handleChecked}) => {
 const StyledCheckBox = styled.div`
   display: flex;
   align-items: center;
+  
+  & input, label {
+    cursor: pointer;
+
+  }
+
+  & label:hover {
+    color: ${COLORS.secondary};
+  }
+
   & input {
     height: 18px;
     margin-right: 8px;
--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -2,7 +2,9 @@ import styled from "styled-components";
 import { COLORS } from "../../constants";
 import { NavLink, useLocation, useParams } from "react-router-dom";
 import { BsSmartwatch } from 'react-icons/bs';
+import { AiFillCaretRight, AiFillCaretDown } from 'react-icons/ai';
 import Checkbox from "./Checkbox";
+import { useState } from "react";
 
 const Sidebar = ({companies, handleChecked}) => {
   const location = useLocation();
@@ -10,6 +12,12 @@ const Sidebar = ({companies, handleChecked}) => {
 
   const params = useParams();
   const category = params.category;
+
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = () => {
+    setIsShown(!isShown);
+  }
 
   return (
     <Wrapper>
@@ -22,13 +30,17 @@ const Sidebar = ({companies, handleChecked}) => {
             <span>{category.includes('and') ? category.replace('and', '&').toUpperCase() : category.toUpperCase()}</span>
           </StyledLink>
         </StyledTitle>
-        <StyledTitle>Companies</StyledTitle>
-        { companies.length > 0 &&
-          companies.map(company => (
-            <Checkbox key={company._id} company={company} handleChecked={handleChecked} />
-          ))
-        }
-      <StyledTitle>Filter</StyledTitle>
+        <StyledTitleBtn onClick={handleClick}><div>Companies</div><div>{
+          !isShown ? <AiFillCaretRight /> : <AiFillCaretDown />
+          }</div></StyledTitleBtn>
+        <InputBox isShown={isShown}>
+          { companies.length > 0 &&
+            companies.map(company => (
+              <Checkbox key={company._id} company={company} handleChecked={handleChecked} />
+            ))
+          }
+        </InputBox>
+      <StyledTitleBtn>Filter</StyledTitleBtn>
     </Wrapper>
   )
 }
@@ -38,6 +50,10 @@ const Wrapper = styled.div`
   min-height: calc(100vh - 85px - 47px);
   border-right: 1px solid ${COLORS.secondary};
   padding: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
 `;
 
 const StyledTitle = styled.div`
@@ -48,7 +64,35 @@ const StyledTitle = styled.div`
   & span:first-child {
     margin-right: 8px;
   }
-  `;
+`;
+
+const StyledTitleBtn = styled.button`
+  font-weight: bold;
+  color: ${COLORS.dark};
+  border: none;
+  background: none;
+  outline: none;
+  font-size: 16px;
+  cursor: pointer;
+  padding: 0;
+  margin: 8px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & div:first-child {
+    margin-right: 4px;
+  }
+  & div:last-child {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const InputBox = styled.div`
+  display: ${({isShown}) => isShown ? 'block' : 'none'};
+  transition: all 300ms ease;
+`;
 
 const StyledLink = styled(NavLink)`
   display: flex;
