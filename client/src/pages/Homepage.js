b02fc28d5e194b383d1dd490c72e94621afe06ba
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-04-03 00:48:52 -0400

    footer, work in progress

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -2,12 +2,13 @@ import MainBanner from "../components/MainBanner";
 import PopularItemsBanner from "../components/PopularItemsBanner";
 import CompaniesBanner from "../components/CompaniesBanner";
 
+
 const Homepage = () => {
   return (
     <>
       <MainBanner />
-      <PopularItemsBanner />
-      <CompaniesBanner />
+      {/* <PopularItemsBanner />
+      <CompaniesBanner /> */}
     </>
   );
 };
